30d65e92045ea7517aec2e4a0f23903536950db1
Brown0101 <dwayne@dontasweb.com>
2017-11-04 18:40:33 -0500

    Changed code to dynamically add a board and mines

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,26 +1,59 @@
-// Prints the formated board
-// array to the conole
-const printBoard = board => {
-  // Logs the Current board state
-  console.log("Current Board:");
-  // Logs the each element of the board
-  // array and joining them with the pipe.
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+// Used to dynamically create our game board
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  // This variable will be used to create a board
+  let board = [];
+  // Based on the players input the board
+  // will be dynamically generated
+  for(let rows = 0; rows < numberOfRows; rows++) {
+    let row = [];
+    for(let columns = 0; columns < numberOfColumns; columns++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
 
-// Default board array
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  // This variable will be used to create a board
+  let board = [];
+  // Based on the players input the board
+  // will be dynamically generated
+  for(let rows = 0; rows < numberOfRows; rows++) {
+    let row = [];
+    for(let columns = 0; columns < numberOfColumns; columns++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  // Now to randomly add bombs to the board
+  let numberOfBombsPlaced = 0;
+  while(numberOfBombsPlaced < numberOfBombs) {
+    // NOTE: As of now a bomb can be placed in a duplicate spot
+    // NOTE: This will be modified later with control code.
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows + 0);
+    console.log(randomRowIndex);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns + 0);
+    console.log(randomColumnIndex);
+    // set our bomb placement
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    // increased the bombs placed by 1
+    numberOfBombsPlaced++;
+  }
+  return board;
+};
 
-// Dsiplay our board to teh console.
-printBoard(board);
+// function used to print our board
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
 
-// Sample user inputs and printing board
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+// setup the game boards
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+// prints out the state of the game board
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
